be7c386509f1765ce5d5616218d8bf3d414b9967
uselessfreemind <96105821+uselessfreemind@users.noreply.github.com>
2022-01-14 16:21:55 +0800

    Add files via upload

---
+++ CH1/1.1.33/src/Matrix.java
@@ -0,0 +1,65 @@
+public class Matrix {
+    public static double dot(double x[], double y[]) {
+        return x[0] * x[1] + y[0] * y[1];
+    }
+
+    public static double[][] mult(double x[][], double y[][]) {
+        if (x[0].length != y.length)
+            return null;
+        double res[][] = new double[x.length][y[0].length];
+        double temp = 0.0;
+        int col = 0;
+        while (col <= y[0].length - 1) //compute col by col
+        {
+            for (int i = 0; i <= x.length - 1; i++)//row
+            {
+                for (int j = 0; j <= x[0].length - 1; j++)//col
+                    temp += x[i][j] * y[j][col];
+                res[i][col] = temp;
+                temp = 0.0;
+            }
+            col++;
+        }
+        return res;
+    }
+
+    public static double[][] transpose(double a[][]) {
+        int m = a.length, n = a[0].length;
+        double result[][] = new double[n][m];
+        for (int i = 0; i <= n - 1; i++)
+            for (int j = 0; j <= m - 1; j++)
+                result[i][j] = a[j][i];
+        return result;
+    }
+
+    public static double[] mult(double m[][], double v[]) {
+       
+
+    }
+
+    public static void main(String[] args) {
+        /*double a[] = {2.0, 3.0};
+        double b[] = {2.5, 1.5};
+        StdOut.printf("%f, ", dot(a, b));driver dot product*/
+
+        /*double a[][] = {{2.0, 2.0}, {1.0, 3.0}, {2.5, 2.25}};
+        double b[][] = {{4.0, 1.0, 1.25}, {1.5, 2.0, 5.20}};
+        double res[][] = mult(a, b);
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < b[0].length; j++) {
+                StdOut.printf("%f, ", res[i][j]);
+                if (j == b[0].length - 1) StdOut.println();
+            }driver matrix multiply*/
+
+       /*double a[][] = {{4.0, 1.0, 1.25}, {1.5, 2.0, 5.20}};
+        double res[][] = transpose(a);
+        for (int i = 0; i < res.length; i++)
+            for (int j = 0; j < res[0].length; j++) {
+                StdOut.printf("%f, ", res[i][j]);
+                if (j == res[0].length - 1) StdOut.println();
+                }
+                driver matrix transpose
+        */
+    }
+}
+
