ed02ff84d6b2e2bdcb0073b6d3597a403cb16eb2
uselessfreemind <96105821+uselessfreemind@users.noreply.github.com>
2025-06-24 10:17:42 +0800

    Add files via upload

--- CH1/1.4/1.4.10/BinSearchV2.java
+++ CH1/1.4/1.4.10/BinSearchV2.java
@@ -1,39 +1,34 @@
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Arrays;
 
 public class BinSearchV2 {
-    private int bs(int ar[], int key, int lo, int hi) {
-        int mid = lo + (hi - lo) / 2;
+
+
+    public int bisch(int[] ar, int key) {
+        int lo = 0, hi = ar.length - 1;
+        int mid, mid_next = -1;
         while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
             if (ar[mid] > key)
                 hi = mid - 1;
             else if (ar[mid] < key)
                 lo = mid + 1;
-            else return mid;
+            else {
+                mid_next = mid;
+                hi = mid - 1;
+            }
         }
-        return -1;
-    }
-
-    public int bisch(int[] ar, int key, int lo, int hi) {
-        int idx = 0;
-        for (int j = 0; j < 19; j++)
-            StdOut.printf("%d, ", ar[j]);
-        idx = bs(ar, key, lo, hi);
-        while (idx != -1)
-            idx = bs(ar, key, lo, idx);
-        return;
-
+        return mid_next;
     }
 
     public static void main(String[] args) {
-        int source[] = new int[20];
-        for (int i = 0; i < 19; i++)
-            source[i] = StdRandom.uniformInt(0, 19);
+        int source[] = {1, 2, 3, 3, 4, 7, 7, 7, 10};
+        // for (int i = 0; i < 19; i++)
+        //  source[i] = StdRandom.uniformInt(0, 19);
         BinSearchV2 a = new BinSearchV2();
         Arrays.sort(source);
-        StdOut.printf("\nThe min index of the key %d is %d", 12, a.bisch(source, 12));
+        StdOut.printf("\nThe min index of the key %d is %d", 12, a.bisch(source, 17));
         ;
 
     }
